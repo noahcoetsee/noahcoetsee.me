3112f88581ff27767bab1b74ecb08f0f0b38d967
Noah Coetsee <noahcoder77@gmail.com>
2022-09-19 23:06:29 -0400

    Continued styling work

--- public/index.js
+++ public/index.js
@@ -1,3 +1,31 @@
+const localStorage = Window.localStorage;
+
 let downloadFullResume = () => {
     
 };
+
+/** Switch site from terminal to normal page and back again */
+let switchBasis = (terminal = false) => {
+  // todo: switching functionality
+  localStorage.setItem("nc-basis", localStorage.getItem("nc-basis") === "page" || terminal ? "terminal" : "page");
+};
+
+let initializeCanvas = () => {
+  const canvas = document.getElementById("header-canvas");
+  if(canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+      
+    const circle = new Path2D();
+    circle.arc(100, 100, 100, 0, 2 * Math.PI);
+    ctx.stroke(circle);
+  }
+ 
+};
+
+
+/** Startup */
+(() => {
+  if (Window.localStorage?.getItem("nc-basis") === "page") {
+    switchBasis(true);
+  }
+})();
